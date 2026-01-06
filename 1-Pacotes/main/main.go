383009f57cfe6434c128c7215baa390c9bf080fa
mattnicee7 <mattxnice@gmail.com>
2026-01-05 21:18:57 -0300

    pacotes externos

--- 1-Pacotes/main/main.go
+++ 1-Pacotes/main/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"AprendizadoGo/1-Pacotes/auxiliar"
 	"fmt"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Hello World")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("matt@gmail.com")
+	fmt.Println(erro)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module AprendizadoGo
 
 go 1.25
+
+require github.com/badoux/checkmail v1.2.4 // indirect
