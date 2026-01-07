3c1b0a1e179bb7c898811473b940c2f0211c7d03
mattnicee7 <mattxnice@gmail.com>
2026-01-07 14:33:44 -0300

    linha de comando - busca por ips

--- aula17_aplicacao-linha-de-comando/app/app.go
+++ aula17_aplicacao-linha-de-comando/app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // gerar vai retornar a aplicacao de linha de comando pronta para ser executada
 func Gerar() *cli.App {
@@ -8,5 +14,32 @@ func Gerar() *cli.App {
 	app.Name = "Aplicacao de Linha de Comando"
 	app.Usage = "Busca IPs e nomes de servidor na internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPs de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
