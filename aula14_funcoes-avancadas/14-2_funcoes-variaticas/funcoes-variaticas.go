4f892857141fd9840cebc7b45de38e0acd337041
mattnicee7 <mattxnice@gmail.com>
2026-01-07 02:00:18 -0300

    funcoes avancadas - funcoes variaticas

---
+++ aula14_funcoes-avancadas/14-2_funcoes-variaticas/funcoes-variaticas.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func soma(numeros ...int) int {
+	total := 0
+	for _, numero := range numeros {
+		total += numero
+	}
+	return total
+}
+
+func escrever(texto string, numeros ...int) {
+	for _, numero := range numeros {
+		fmt.Println(texto, numero)
+	}
+}
+
+func main() {
+	totalDaSoma := soma(1, 2, 3, 4, 5, 6)
+	fmt.Println(totalDaSoma)
+
+	escrever("ola mundo", 10, 2, 3, 4, 5, 6, 7)
+}
