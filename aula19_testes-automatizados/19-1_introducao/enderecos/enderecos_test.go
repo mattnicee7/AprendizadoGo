aba2396057c75a0aa804898d63bc481532086c94
mattnicee7 <mattxnice@gmail.com>
2026-01-08 21:26:02 -0300

    teste unitario multiplo

--- aula19_testes-automatizados/19-1_introducao/enderecos/enderecos_test.go
+++ aula19_testes-automatizados/19-1_introducao/enderecos/enderecos_test.go
@@ -2,17 +2,33 @@ package enderecos
 
 import "testing"
 
+type cenarioDeTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
 // teste de unidade
 // Test+Nome da funcao
 // arquivo precisa ter _test.go
 func TestTipoDeEndereco(t *testing.T) {
-	enderecoParaTeste := "Avenida Paulista"
+	cenariosDeTeste := []cenarioDeTeste{
+		{"Rua Abc", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia dos Imigrantes", "Rodovia"},
+		{"Praça das Rosas", "Invalido"},
+		{"Estrada qualquer", "Estrada"},
+		{"rua dos bobos", "Rua"},
+		{" ", "Invalido"},
+	}
 
-	tipoDeEnderecoEsperado := "Avenida"
-
-	tipoDeEnderecoRecebido := TipoDeEndereco(enderecoParaTeste)
-	if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
-		t.Error("O tipo recebido é diferente do esperado")
+	for _, cenario := range cenariosDeTeste {
+		tipoDeEnderecoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+		if tipoDeEnderecoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s",
+				tipoDeEnderecoRecebido,
+				cenario.retornoEsperado,
+			)
+		}
 	}
 
 }
