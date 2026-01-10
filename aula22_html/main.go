c998abab6879be2f8d00345279ceaf1644376820
mattnicee7 <mattxnice@gmail.com>
2026-01-10 14:33:03 -0300

    html - renderização

--- aula22_html/main.go
+++ aula22_html/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
+var templates *template.Template
+
+type usuario struct {
+	Nome  string
+	Email string
 }
 
 func main() {
-	http.HandleFunc("/home", home)
+	templates = template.Must(template.ParseGlob("*.html"))
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		u := usuario{"Joao", "joao.pedro@gmail.com"}
 
+		if err := templates.ExecuteTemplate(w, "home.html", u); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Println("template error:", err)
+		}
+	})
+
+	fmt.Println("Escutando na porta 5000")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
