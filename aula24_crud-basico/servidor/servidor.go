3e0e1cb388e350e9d77a518e1b713ea9d63a0768
mattnicee7 <mattxnice@gmail.com>
2026-01-15 14:39:27 -0300

    crud - buscar usuarios

--- aula24_crud-basico/servidor/servidor.go
+++ aula24_crud-basico/servidor/servidor.go
@@ -57,3 +57,44 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf("Usuario inserido com sucesso. Id: %d", idInserido)))
 }
+
+// BuscarUsuarios retorna todos usuários
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o Banco"))
+	}
+
+	defer db.Close()
+
+	linhas, erro := db.Query("SELECT * FROM  usuarios")
+
+	if erro != nil {
+		w.Write([]byte("Erro ao buscar usuarios"))
+	}
+
+	defer linhas.Close()
+
+	var usuarios []usuario
+	for linhas.Next() {
+		var usuario usuario
+
+		if erro := linhas.Scan(&usuario.ID, &usuario.Nome, &usuario.Email); erro != nil {
+			w.Write([]byte("Erro ao escanear o usuario"))
+			return
+		}
+
+		usuarios = append(usuarios, usuario)
+		// 1	 joao 	joao@gmail.com
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if erro := json.NewEncoder(w).Encode(usuarios); erro != nil {
+		w.Write([]byte("Erro ao converter usuarios para json"))
+	}
+}
+
+// BuscarUsuario retorna um usuário especifico
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+
+}
