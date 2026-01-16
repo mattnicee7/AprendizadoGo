d0b7a592a7d325bbd77129adfda8ed0e188e1973
mattnicee7 <mattxnice@gmail.com>
2026-01-16 01:26:07 -0300

    crud - atualizar usuario pelo id

--- aula24_crud-basico/servidor/servidor.go
+++ aula24_crud-basico/servidor/servidor.go
@@ -135,3 +135,46 @@ func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+
+	ID, erro := strconv.ParseUint(parametros["id"], 10, 32)
+	if erro != nil {
+		w.Write([]byte("Erro ao converter o parametro para inteiro"))
+		return
+	}
+
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		w.Write([]byte("Erro ao ler o corpo da requisicao"))
+		return
+	}
+
+	var usuario usuario
+	if erro := json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		w.Write([]byte("Erro ao converter o usuario para struct"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o Banco"))
+		return
+	}
+
+	defer db.Close()
+	statement, erro := db.Prepare("UPDATE usuarios SET nome=?, email=? WHERE id=?")
+	if erro != nil {
+		w.Write([]byte("Erro ao criar o statement"))
+		return
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(usuario.Nome, usuario.Email, ID); erro != nil {
+		w.Write([]byte("Erro ao atualizar usuario"))
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
